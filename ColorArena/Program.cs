bd46e90bbdc57b4cdea2ccbe80422dd88b9c94a1
edrianson <101627921@student.swin.edu.au>
2017-11-13 21:10:45 +1100

    Reword 90d9a79, refactor, add demo sequence

--- ColorArena/Program.cs
+++ ColorArena/Program.cs
@@ -7,28 +7,39 @@ namespace ColorArena
     class Program
     {
         public static Brick brick { get; set; }
+        public static float proximity { get; set; }
+        public static float color { get; set; }
 
         static void Main(string[] args)
         {
             Connect();
 
-            brick.BrickChanged += DistanceChanged;
-            brick.BrickChanged += ColorChanged;
+            brick.BrickChanged += UltrasonicChanged;
+            brick.BrickChanged += PhotodiodeChanged;
 
-            brick.BrickChanged += CollisionAvoidance;
+			// Demo sequence
+            Console.WriteLine("Moving forward.");
+            Move(20, 20);
 
-            Forward();
+            while (proximity > 5) { }
+            Console.WriteLine("Turning left.");
+            Move(20, -20);
 
-            /* :loop
-             * Move forward() indefinitely,
+            while (proximity < 5) { }
+            Stop();
+
+            Console.WriteLine("EOF");
+            Console.ReadKey();
+
+            /* Move forward() indefinitely,
              * stop in proximity of a colored arena wall,
              * if color == base_color then stop, else continue
-             * to rotate() until no obstacle in path,
-             * goto loop
+             * to rotate() until no obstacle in path, rendering
+             * unnecessary: the drifting-prone gyroscope and brute-forcing
+             * of imprecise motor power/speed parameters to
+             * reproduce the desired angle.
+             * Loop
              */
-
-            Console.WriteLine("Sequence complete!");
-            Console.ReadKey();
         }
 
         static async void Connect()
@@ -37,45 +48,36 @@ namespace ColorArena
             await brick.ConnectAsync();
         }
 
-        // Rotate both motors indefinitely to move forward.
-        static async void Forward()
+        // Rotate both motors indefinitely to move.
+        // Direction is dictated by powerA/powerD parameters.
+        static async void Move(int powerA, int powerD)
         {
             brick.BatchCommand.StartMotor(OutputPort.A);
-            brick.BatchCommand.TurnMotorAtPower(OutputPort.A, 20);
+            brick.BatchCommand.TurnMotorAtPower(OutputPort.A, powerA);
 
             brick.BatchCommand.StartMotor(OutputPort.D);
-            brick.BatchCommand.TurnMotorAtPower(OutputPort.D, 20);
+            brick.BatchCommand.TurnMotorAtPower(OutputPort.D, powerD);
 
             await brick.BatchCommand.SendCommandAsync();
         }
 
-        // Stop both motors when in close proximity of an obstacle.
-        static async void CollisionAvoidance(object sender, BrickChangedEventArgs e)
+        static async void Stop()
         {
-            if (e.Ports[InputPort.Two].SIValue < 10)
-            {
-                brick.BatchCommand.StopMotor(OutputPort.A, true);
-                brick.BatchCommand.StopMotor(OutputPort.D, true);
+            brick.BatchCommand.StopMotor(OutputPort.A, true);
+            brick.BatchCommand.StopMotor(OutputPort.D, true);
 
-                await brick.BatchCommand.SendCommandAsync();
-            }
+            await brick.BatchCommand.SendCommandAsync();
         }
 
-        // Rotate until path ahead is clear; rendering unnecessary: the gyroscope, and
-        // brute-forcing of imprecise motor power/speed parameters to reproduce the desired angle.
-        //static async void Rotate() { }
-
-        // Raw ultrasonic values
-        static void DistanceChanged(object sender, BrickChangedEventArgs e)
+        static void UltrasonicChanged(object sender, BrickChangedEventArgs e)
         {
-            Console.WriteLine(e.Ports[InputPort.Two].SIValue.ToString());
+            proximity = e.Ports[InputPort.Two].SIValue;
         }
 
-        // Raw color sensor values
-        static void ColorChanged(object sender, BrickChangedEventArgs e)
+        static void PhotodiodeChanged(object sender, BrickChangedEventArgs e)
         {
             brick.Ports[InputPort.Four].SetMode(ColorMode.Color);
-            Console.WriteLine(e.Ports[InputPort.Four].SIValue.ToString());
+            color = e.Ports[InputPort.Four].SIValue;
         }
     }
 }
