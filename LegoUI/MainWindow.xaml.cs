0c5865fa74948b8625ce5bcb16ad3b9332037840
Bryce Stevens <brycestevens@y7mail.com>
2017-11-24 14:07:53 +1100

    Added UI, not implement yet.

---
+++ LegoUI/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LegoUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            counter = 0;
+            
+        }
+
+        private void btnWhiteCube_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+        private void btnRedBlue_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnBlueYellow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRedBlack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBlackYellow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
