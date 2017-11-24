97de04afd4152a32d6debdae66e82852aa315794
101119897 <101119897@TA207-007>
2017-11-24 14:55:22 +1100

    Merged gryo and color sensor together, also swapped bluetoothcommunications for UsbCommunication

--- ColorArena/Program.cs
+++ ColorArena/Program.cs
@@ -9,38 +9,58 @@ namespace ColorArena
         public static Brick brick { get; set; }
         public static float proximity { get; set; }
         public static float color { get; set; }
-        public static float angle { get; set; }
+        public static float angle {get; set;}
 
         static void Main(string[] args)
         {
-            BluetoothConnect();
+            Connect();
 
-            brick.BrickChanged += DistanceChanged;
-            brick.BrickChanged += ColorChanged;
-            brick.BrickChanged += GyroChanged;
+            Console.Write("Base color: ");
+            int base_color = Convert.ToInt32(Console.ReadLine());
 
-            
+            // Event handlers to store sensor proximity, color values.
+            brick.BrickChanged += UltrasonicChanged;
+            brick.BrickChanged += PhotodiodeChanged;
+	    brick.BrickChanged += GyroChanged;
 
-            
+            // Demo sequence
+            // Loops until base_color is detected.
+            while (color != base_color)
+            {
+                while (proximity > 20 && color != base_color){}
+                
+                    Move(20, 20);
+                    Console.WriteLine("Path Clear");
+                
+                while (proximity > 10 && color != base_color){}
+                
+                    Move(20, -20);
+                    Console.WriteLine("Obstacle encountered.");
+                
+                
+            }
+            Console.WriteLine("Base found.");
 
-            
-
-            Console.WriteLine("Sequence complete!");
             Console.ReadKey();
+
+            /* Move forward indefinitely,
+             * rotate when in close proximity until path clear,
+             * loop if color != base_color, else stop.
+             * 
+             * (this renders unnecessary: the drifting-prone gyroscope and brute-forcing
+             * of imprecise motor power/speed parameters to reproduce the desired angle.)
+             */
         }
 
         static async void Connect()
         {
+           // brick = new Brick(new BluetoothCommunication("com4"));
             brick = new Brick(new UsbCommunication());
             await brick.ConnectAsync();
         }
 
-        static async void BluetoothConnect()
-        {
-            brick = new Brick(new BluetoothCommunication("com4"));
-            await brick.ConnectAsync();
-        }
-        // Rotate both motors indefinitely to move forward.
+        // Rotate both motors indefinitely to move.
+        // powerA/powerD parameters dictate speed and direction.
         static async void Move(int powerA, int powerD)
         {
             brick.BatchCommand.StartMotor(OutputPort.A);
@@ -52,29 +72,29 @@ namespace ColorArena
             await brick.BatchCommand.SendCommandAsync();
         }
 
-        
-        
+        static async void Stop()
+        {
+            brick.BatchCommand.StopMotor(OutputPort.A, true);
+            brick.BatchCommand.StopMotor(OutputPort.D, true);
 
-        // Rotate until path ahead is clear; rendering unnecessary: the gyroscope, and
-        // brute-forcing of imprecise motor power/speed parameters to reproduce the desired angle.
-        //static async void Rotate() { }
+            await brick.BatchCommand.SendCommandAsync();
+        }
 
-        // Raw ultrasonic values
-        static void DistanceChanged(object sender, BrickChangedEventArgs e)
+        static void UltrasonicChanged(object sender, BrickChangedEventArgs e)
         {
             proximity = e.Ports[InputPort.Two].SIValue;
         }
 
-        // Raw color sensor values
-        static void ColorChanged(object sender, BrickChangedEventArgs e)
+        static void PhotodiodeChanged(object sender, BrickChangedEventArgs e)
         {
             brick.Ports[InputPort.Four].SetMode(ColorMode.Color);
             color = e.Ports[InputPort.Four].SIValue;
         }
 
-        static void GyroChanged(object sender, BrickChangedEventArgs e)
+ 	        static void GyroChanged(object sender, BrickChangedEventArgs e)
         {
             angle = e.Ports[InputPort.One].SIValue;
         }
+
     }
 }
